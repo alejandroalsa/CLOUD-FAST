8825a6f09b5faf2e425863d8bb37eebd3782ef98
alejandroalsa <alejandro@alejandroalsa.es>
2023-02-26 21:15:04 +0100

    Solución del error: Edición de texto
    Signed-off-by: alejandroalsa <alejandro@alejandroalsa.es>

--- actions/update/update-texts.php
+++ actions/update/update-texts.php
@@ -17,12 +17,11 @@
     $url = "../../sections/text.php";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_GET["id"]) || empty($_POST["z"]) || empty($_POST["textArea"])) {
+        if (empty($_GET["id"]) || empty($_POST["textTitle"]) || empty($_POST["textArea"])) {
             $_SESSION["alert"] = ["estilo" => "danger", "icono" => "bi bi-exclamation-triangle-fill", "msg" => "El texto no se ha podido actualizar", "title" => "ERROR"];
             header("Location: $url");
             return;  
         }
-
 
         $text_edit = $con->prepare("UPDATE texts SET texts_title = :texts_title, texts = :texts WHERE id = :id");
         $text_edit->execute([
